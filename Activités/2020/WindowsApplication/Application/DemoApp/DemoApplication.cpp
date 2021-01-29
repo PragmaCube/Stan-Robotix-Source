8786313923c9d6972ca59bb014f29ee78620d337
QuantaHD <raph17.poth@gmail.com>
2021-01-29 18:02:14 -0500

    Rajout de la première doc et correction du readme

--- Activités/2020/WindowsApplication/Application/DemoApp/DemoApplication.cpp
+++ Activités/2020/WindowsApplication/Application/DemoApp/DemoApplication.cpp
@@ -35,14 +35,14 @@ void DemoApplication::onChar(char iChar, short iDetail)
 {
 	if (iChar == '-')
 	{
-		if (mNbActiveBall > 2)
+		if (mNbActiveBall > 1)
 		{
 			mNbActiveBall--;
 		}
 	}
 	else if(iChar == '+')
 	{
-		if (mNbActiveBall < 8)
+		if (mNbActiveBall < 80)
 		{
 			mNbActiveBall++;
 		}
@@ -59,7 +59,7 @@ void DemoApplication::onChar(char iChar, short iDetail)
 				break;
 			case 's':
 			case 'S':
-				mDemoAppBall[i] .increaseRadius();
+				mDemoAppBall[i].increaseRadius();
 				break;
 			case 'q':
 			case 'Q':
@@ -96,9 +96,9 @@ void DemoApplication::onMouseRightClick(int iPosX, int iPosY)
 
 void DemoApplication::onTimer()
 {
-	for (int i = 0; i < 8; i++)
+	/*for (int i = 0; i < 8; i++)
 	{
 		mDemoAppBall[i].applyTime();
-	}
+	}*/
 	IApplication::onTimer(); // Pour redessiner l'ecran
 }
