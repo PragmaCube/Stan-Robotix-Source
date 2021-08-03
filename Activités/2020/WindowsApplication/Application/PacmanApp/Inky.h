8afa16b4b770d306251a0ce25839dcbfd5fa2f22
QuantaHD <raph17.poth@gmail.com>
2021-08-03 18:12:31 -0400

    Nettoyage des fichiers Inky.h et Inky.cpp. Raphaël

--- Activités/2020/WindowsApplication/Application/PacmanApp/Inky.h
+++ Activités/2020/WindowsApplication/Application/PacmanApp/Inky.h
@@ -11,8 +11,7 @@ Personnalité : on double le vecteur Blinky-Cible et on obtient la cible d'Inky
 class Inky :
 	public PacmanMonster
 {
-	unsigned int mTargetX1 = 1, mTargetY1 = 1, mTargetX2 = 26, mTargetY2 = 24;
-	bool mTarget1Done = false, mTarget2Done = false, mInBox = true;
+	bool mInBox = true;
 
 	char mExitPath[16] = { 'u', 'u', 'u', 'l', 'u', 'u', 'l', 'l', 'l', 'u', 'u', 'u', 'l', 'l', 'l' };
 
@@ -29,9 +28,6 @@ class Inky :
 public:
 	Inky();
 
-	char bestRightLeft(char iLastChoice, unsigned int iOtherY);
-	char bestUpDown(char iLastChoice, unsigned int iOtherX);
-	char nextMoveWithTarget();
 	void path();
 	void move();
 };
