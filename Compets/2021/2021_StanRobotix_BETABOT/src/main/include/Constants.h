b2d984232551c0e7b6bd608704edb830af5c3ba7
QuantaHD <raph17.poth@gmail.com>
2021-10-26 13:40:05 -0400

    Ajout du répertoire pour la compétition Betabot 2021

---
+++ Compets/2021/2021_StanRobotix_BETABOT/src/main/include/Constants.h
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants.  This should not be used for any other
+ * purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+// À modifier si besoin
+
+constexpr int kMotorR1Port = 0;
+constexpr int kMotorR2Port = 1;
+constexpr int kMotorL1Port = 2;
+constexpr int kMotorL2Port = 3;
