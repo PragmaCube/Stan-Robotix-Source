beae8466da4f416e9c02a9c89976cf10bf4853b8
Mismis51 <blanarutoma@yahoo.ca>
2022-03-25 17:45:22 -0400

    pid wpilib

---
+++ Compets/2022/2022_StanRobotix_FRC/src/main/include/subsystems/pid_subsystem.h
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc2/command/PIDSubsystem.h>
+#include "Constants.h"
+
+class PidSubsystem : public frc2::SubsystemBase {
+ public:
+  PidSubsystem(double kP, double kI, double kD);
+  ~PidSubsystem();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs during
+   * simulation.
+   */
+  void SimulationPeriodic() override;
+
+  void SetPoint(double iSetPoint);
+
+  void UseOutput(double iError);
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+
+  // Utilisation PID
+  frc2::PIDController* mPid;
+  double _getOutput(double iError);
+  double mSetPoint;
+
+  // Accéléromètre
+  frc::BuiltInAccelerometer m_accelerometer;
+  std::chrono::steady_clock::time_point m_last_time;
+
+  // Création des moteurs
+  frc::Talon mMotorL1{kMotorL1Port};
+  frc::Talon mMotorL2{kMotorL2Port};
+  frc::Talon mMotorR1{kMotorR1Port};
+  frc::Talon mMotorR2{kMotorR2Port}; 
+
+  frc::SpeedControllerGroup mLeftSide{mMotorL1, mMotorL2};
+	frc::SpeedControllerGroup mRightSide{mMotorR1, mMotorR2};
+
+  frc::DifferentialDrive mDriveTrain{mLeftSide, mRightSide};
+};
