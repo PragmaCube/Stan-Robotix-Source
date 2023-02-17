73c5f5fcb6059300982ca3aedb24535b6e46ead5
ein Berliner <stan4eme4ilot1@gmail.com>
2023-02-17 17:28:45 -0500

    avancement contactdetection à continuer

--- Compets/2023/2023_StanRobotix_FRC/FirstRobot6622/src/main/cpp/subsystems/SubContactDetection.cpp
+++ Compets/2023/2023_StanRobotix_FRC/FirstRobot6622/src/main/cpp/subsystems/SubContactDetection.cpp
@@ -4,13 +4,40 @@
 
 #include "subsystems/SubContactDetection.h"
 
-SubContactDetection::SubContactDetection() = default;
+SubContactDetection::SubContactDetection() {
+   mTototo.reserve(10);
+   
 
+   // parcourir la liste kContactEnable[10] avec for (;;)
+   // si l element index = true alors 
+   for(int i = 0; i < 10; i++)
+   {
+    if (mContactStatus[i] == true)
+    {
+        break;
+    }
+   } 
 
-bool SubContactDetection::GetContactStatus () {
-    return mContactStatus.Get();
+   mTototo[index] = new frc::DigitalInput(index);
+
+}
+
+bool SubContactDetection::GetContactStatus (int index) {
+   // return mContactStatus.Get();
+
+    return mContactStatus[index % 10] ;
+   
 }
 
 
 // This method will be called once per scheduler run
-void SubContactDetection::Periodic() {}
+void SubContactDetection::Periodic() 
+{
+    for(int i=0;i<10 ; i++)
+    {
+        mContactStatus [i]=;
+
+    }
+    // Pour chaque  kContactEnableîç == true
+    // mContactStatus[index] = mContactSmTototo[index]->Get(); 
+}
