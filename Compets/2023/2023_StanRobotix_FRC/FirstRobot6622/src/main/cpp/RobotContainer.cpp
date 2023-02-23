8c2c74c8493870507b96c857a483824eccba6b38
StanRobotix <taco@trello.com>
2023-02-23 09:04:07 -0500

    Reactivation des modules en mode teleopere.. sur plateforme de test

--- Compets/2023/2023_StanRobotix_FRC/FirstRobot6622/src/main/cpp/RobotContainer.cpp
+++ Compets/2023/2023_StanRobotix_FRC/FirstRobot6622/src/main/cpp/RobotContainer.cpp
@@ -12,16 +12,16 @@ RobotContainer::RobotContainer()
 
   // Liste des sorties
   mSubDriveTrain      = new SubDriveTrain(this);
-  mSubElevator        = new SubElevator(this);
+  //mSubElevator        = new SubElevator(this); // TODO
   mSubPneumatic       = new SubPneumatic(this);
 
   // liste des entrees.
   mSubColorSensor     = new SubColorSensor();
   mSubImu             = new SubIMU();
-  mSubLimelight       = new SubLimelight();
+  mSubLimelight       = new SubLimelight(); 
   mSubContactDetection= new SubContactDetection();
-  mSubUltrasonic      = new SubUltrasonic();
-  mSubUltrasonic->EnableImperialSystem();
+  //mSubUltrasonic      = new SubUltrasonic();   // TODO
+  //mSubUltrasonic->EnableImperialSystem();      // TODO
   
   ConfigureButtonBindings();
 }
@@ -30,13 +30,13 @@ void RobotContainer::AutonomousInit()
 {
   // Initialisation des subsystemes
   mSubDriveTrain->Init();
-  mSubElevator->Init();
+  //mSubElevator->Init();    // TODO
   mSubPneumatic->Init();
   mSubImu->Init();
-  mSubLimelight->Init();
+  //mSubLimelight->Init();// TODO
   mSubColorSensor->Init();
   mSubContactDetection->Init();
-  mSubUltrasonic->Init();
+ // mSubUltrasonic->Init();// TODO
 
   // Initialisation des commandes automatisees.
   m_autonomousCommand->Init();
@@ -56,7 +56,7 @@ void RobotContainer::TeleopInit()
 }
 
 void RobotContainer::DriveDisplacement()
-{
+{// TODO
   const double slider = (1 - mJoystick.GetThrottle()) / 2;
   mSubDriveTrain->MoveMeca(mJoystick.GetX() * slider, mJoystick.GetY() * slider, mJoystick.GetTwist() * slider, 1 - mJoystick.GetRawButton(1));
   // if (mJoystick.GetRawButtonPressed(2))
@@ -76,15 +76,15 @@ void RobotContainer::Drive()
 
   //mSubUltrasonic->Execute(); TODO Enable it!!!!
 
-  mSubElevator->setCommand(mJoystick.GetPOV());
+  //mSubElevator->setCommand(mJoystick.GetPOV()); // TODO
 
   mSubImu->Execute();
 
   mSubColorSensor->Execute();
 
-  mSubLimelight->doExecute();
+  //mSubLimelight->doExecute(); // TODO
 
-  /*if (mJoystick.GetRawButtonPressed(1))
+  /*if (mJoystick.GetRawButtonPressed(1)) // TODO
   {
     mSubPneumatic->Toggle();
   }*/
