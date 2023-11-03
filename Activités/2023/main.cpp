df406a50e540167a127d9f354182e77b695900ea
Stan-Robotix <stanrobotix@stanislas.qc.ca>
2023-11-03 19:45:38 -0400

    Ajout du groupe 1

--- Activités/2023/main.cpp
+++ Activités/2023/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "sonic.h"
 using namespace std;
 
 int main()
 {
-    cout << "Bonsoir !";
+  Sonic sonic;
 
-    return 0;
+  cout << "Sonic a parcouru " << sonic.distanceParcourue(7200) << " metres en 2h";
 }
+
