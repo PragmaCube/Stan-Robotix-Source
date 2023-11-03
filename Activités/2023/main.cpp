b01b4c1109438eccd94dc01ff71501d8816986c2
Stan Robotix <stanrobotix@stanislas.qc.ca>
2023-11-03 19:39:48 -0400

    ouaiiisss

--- Activités/2023/main.cpp
+++ Activités/2023/main.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+/* #include <iostream>
+#include "point.h"
+ using namespace std;
 
-class neurones {
-  vector<float> x;
-  vector<float> y;
 
-public:
-  neurones();
-  ~neurones();
 
-  void setX(float x);
-  vector<float> getX() { return x; }
 
-  void setY(float y);
-  vector<float> getY() { return y; }
 
-  for (int i = 0; i < x.size(); i++) {
-  }
-};
 
-int main() {}
+int main()
+{
+
+}
+
+*/
