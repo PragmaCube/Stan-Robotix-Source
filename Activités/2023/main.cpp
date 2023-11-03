89d6dd91acd7d5ef37cc9b650407fc8e1f10cc50
Stan Robotix <stanrobotix@stanislas.qc.ca>
2023-11-03 19:41:34 -0400

    ouaiiisss

--- Activités/2023/main.cpp
+++ Activités/2023/main.cpp
@@ -1,15 +1,25 @@
-/* #include <iostream>
-#include "point.h"
- using namespace std;
-
-
-
-
-
-
+class Sonic
+{
+const float vitesse = 342.88;
+public:
+Sonic();
+~Sonic();
+float getVitesse();
+float distanceParcourue(float secondes);
+};
+Sonic::Sonic() { }
+Sonic::~Sonic() { }
+float Sonic::getVitesse()
+{
+return vitesse;
+}
+float Sonic::distanceParcourue(float secondes)
+41
+{
+return vitesse * secondes;
+}
 int main()
 {
-
+Sonic sonic;
+cout << "Sonic a parcouru " << sonic.distanceParcoure(7200) << " mètres en 2h";
 }
-
-*/
