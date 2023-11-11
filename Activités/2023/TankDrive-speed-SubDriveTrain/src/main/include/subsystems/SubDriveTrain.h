20bcc8516f1788b093e408abac480f22de555190
wjckandre <wojcik.andre2.0@gmail.com>
2023-11-10 19:32:25 -0500

    Implementé les sparks; motor controller group ; et début tankdrive

---
+++ Activités/2023/TankDrive-speed-SubDriveTrain/src/main/include/subsystems/SubDriveTrain.h
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/motorcontrol/Spark.h>
+#include "Constants.h"
+#include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc/drive/DifferentialDrive.h>
+
+class SubDriveTrain : public frc2::SubsystemBase {
+ public:
+  SubDriveTrain();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+  
+  void TankDrive();
+
+  void TankDrive() override;
+ 
+ private:
+
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  frc::Spark SparkAG{DriveTrainConstants::AvantGauche}; //spark avant gauche 
+  frc::Spark SparkAD{DriveTrainConstants::AvantDroite}; //spark avant droite 
+  frc::Spark SparkDG{DriveTrainConstants::ArriereGauche}; //spark avant gauche 
+  frc::Spark SparkDD{DriveTrainConstants::ArriereDroite}; //spark avant droite 
+  
+  
+  frc::MotorControllerGroup Droite{SparkAD, SparkDD};// Groupe Droite
+  frc::MotorControllerGroup Gauche{SparkAG, SparkDG};// Groupe Gauche
+  frc::DifferentialDrive Drive{Gauche, Droite};//Tank drive
+};
+
+
+
+
+
+
+
