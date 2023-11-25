41f68d398d332e8ac85f2062ad678715b64092cc
wjckandre <wojcik.andre2.0@gmail.com>
2023-11-24 19:33:41 -0500

    debut gyro + accel + angle subsysteme IMU

--- Compets/2023/2023_StanRobotix_Betabot/src/main/include/subsystems/SubIMU.h
+++ Compets/2023/2023_StanRobotix_Betabot/src/main/include/subsystems/SubIMU.h
@@ -3,19 +3,34 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-
+#include <ctre/Phoenix.h>
 #include <frc2/command/SubsystemBase.h>
+#include <ctre/phoenix/sensors/Pigeon2.h>
 
-class SubIMU : public frc2::SubsystemBase {
+class SubIMU : public frc2::SubsystemBase 
+{
+   
+
+
+
  public:
   SubIMU();
+
+double getAccelX();
+double getAccelY();
+
+double angle
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
    */
   void Periodic() override;
 
+
+
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+
+  ctre::phoenix::sensors::WPI_Pigeon2 IMU;
 };
