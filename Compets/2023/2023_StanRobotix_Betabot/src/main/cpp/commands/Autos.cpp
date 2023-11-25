f30a3e8ba9d038e8710f5c7197f0c782bdff9e0f
QuantaHD <raph17.poth@gmail.com>
2023-11-24 19:29:28 -0500

    autono command ajout de l'exampl must add librairy

--- Compets/2023/2023_StanRobotix_Betabot/src/main/cpp/commands/Autos.cpp
+++ Compets/2023/2023_StanRobotix_Betabot/src/main/cpp/commands/Autos.cpp
@@ -2,13 +2,142 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/Autos.h"
+#include "include\commands\Autos.h"
+#include "include\RobotContainer.h"
 
-#include <frc2/command/Commands.h>
+#include <iostream>
 
-#include "commands/ExampleCommand.h"
+AutoCmd::AutoCmd(RobotContainer * iRobotContainer)
+{
+   mCurrentStep = Phase1_;
 
-frc2::CommandPtr autos::ExampleAuto(ExampleSubsystem* subsystem) {
-  return frc2::cmd::Sequence(subsystem->ExampleMethodCommand(),
-                             ExampleCommand(subsystem).ToPtr());
+   mRobotContainer = iRobotContainer;
+
+   mGenericTimer.Reset();
+
+   EnableSubsystemLog(kLogAutoCmdEnable);
+   EnablePerformanceLog(kLogPerf_AutoCmdEnable);
+
+   Init();
 }
+
+/**
+ * The initial subroutine of a command.  Called once when the command is
+ * initially scheduled.
+ */
+void AutoCmd::Init()
+{
+   mCurrentStep = Phase1_;
+
+   mSubDriveTrain = mRobotContainer->getSubDriveTrain();
+   mSubLimelight = mRobotContainer->getSubLimelight();
+}
+
+/**
+ * The main body of a command.  Called repeatedly while the command is
+ * scheduled.
+ */
+void AutoCmd::doExecute()
+{
+   bool wIsFinished = true;
+   step_t wNextPhase = mCurrentStep;
+   
+   switch (mCurrentStep)
+   {
+   case Phase1_:
+      doExecutePhase1();
+      wIsFinished = isPhase1Finished();
+      wNextPhase = step_t::PhaseFinish;
+      break;
+
+   case Phase2_:
+      doExecutePhase2();
+      wIsFinished = isPhase2Finished();
+      wNextPhase = step_t::phase3_;
+      break;
+
+   case phase3_:
+      doExecutePhase3();
+      wIsFinished = isPhase3Finished();
+      wNextPhase = step_t::Phase4_;
+      break;
+
+   case Phase4_:
+      doExecutePhase4();
+      wIsFinished = isPhase4Finished();
+      wNextPhase = step_t::Phase5_;
+      break;
+   case Phase5_:
+      doExecutePhase5();
+      wIsFinished = isPhase5Finished();
+      wNextPhase = step_t::Phase6_;
+      break;
+   case Phase6_:
+      doExecutePhase6();
+      wIsFinished = isPhase6Finished();
+      wNextPhase = step_t::Phase7_;
+      break;
+   case Phase7_:
+      doExecutePhase7();
+      wIsFinished = isPhase7Finished();
+      wNextPhase = step_t::Phase8_;
+      break;
+   case Phase8_:
+      doExecutePhase8();
+      wIsFinished = isPhase8Finished();
+      wNextPhase = step_t::Phase9_;
+      break;
+   case Phase9_:
+      doExecutePhase9();
+      wIsFinished = isPhase9Finished();
+      wNextPhase = step_t::PhaseFinish;
+      break;
+   case PhaseFinish:
+      break;
+   };
+
+   if (wIsFinished)
+   {
+      mCurrentStep = wNextPhase;
+   }
+}
+
+bool AutoCmd::isFinish()
+{
+  return mCurrentStep == PhaseFinish;
+}
+
+void AutoCmd::reset() 
+{ 
+   std :: cout <<  "Fin de l'execution de AutoCmd" << std::endl;
+   mCurrentStep = Phase1_; 
+} 
+
+void AutoCmd::doExecutePhase1()
+{
+   std :: cout <<  "Execution de  AutoCmd" << std::endl;
+}
+
+bool AutoCmd::isPhase1Finished()
+{
+   return true;
+}
+
+void AutoCmd::doExecutePhase2() { }
+bool AutoCmd::isPhase2Finished() { return false; }
+void AutoCmd::doExecutePhase3() { }
+bool AutoCmd::isPhase3Finished() { return false; }
+void AutoCmd::doExecutePhase4() { }
+bool AutoCmd::isPhase4Finished() { return false; }
+void AutoCmd::doExecutePhase5() { }
+bool AutoCmd::isPhase5Finished() { return false; }
+void AutoCmd::doExecutePhase6() { }
+bool AutoCmd::isPhase6Finished() { return false; }
+void AutoCmd::doExecutePhase7() { }
+bool AutoCmd::isPhase7Finished() { return false; }
+void AutoCmd::doExecutePhase8() { }
+bool AutoCmd::isPhase8Finished() { return false; }
+void AutoCmd::doExecutePhase9() { }
+bool AutoCmd::isPhase9Finished() { return false; }
+
+void AutoCmd::doFinish() { }
--- Compets/2023/2023_StanRobotix_Betabot/src/main/include/commands/Autos.h
+++ Compets/2023/2023_StanRobotix_Betabot/src/main/include/commands/Autos.h
@@ -3,14 +3,69 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
 
-#include <frc2/command/CommandPtr.h>
 
-#include "subsystems/ExampleSubsystem.h"
+#include "../subsystems/PerformanceMonitor.h"
+#include "subsystems/SubDriveTrain.h"
+#include "subsystems\SubArm.h"
+#include "subsystems\SubEjector.h"
+#include "subsystems\SubIMU.h"
 
-namespace autos {
-/**
- * Example static factory for an autonomous command.
- */
-frc2::CommandPtr ExampleAuto(ExampleSubsystem* subsystem);
-}  // namespace autos
+// AJOUT COMMANDE AUTOMATISEE
+class AutoCmd : public PerformanceMonitor
+{
+public:
+  AutoCmd(RobotContainer *iRobotContainer);
+
+  virtual std::string getName() { return "Autocmd"; }
+  
+  virtual void reset();
+  virtual void doExecute();
+  virtual bool isFinish();
+
+private:
+  enum step_t
+  {
+    Phase1_,
+    Phase2_,
+    phase3_,
+    Phase4_,
+    Phase5_,
+    Phase6_,
+    Phase7_,
+    Phase8_,
+    Phase9_,
+    PhaseFinish
+  };
+
+  step_t          mCurrentStep = Phase1_;
+  frc::Timer      mGenericTimer;
+  SubDriveTrain * mSubDriveTrain = nullptr;
+  SubLimelight *  mSubLimelight = nullptr;
+
+  void Init();
+
+  // Definition des phases
+  void doExecutePhase1();
+  void doExecutePhase2();
+  void doExecutePhase3();
+  void doExecutePhase4();
+  void doExecutePhase5();
+  void doExecutePhase6();
+  void doExecutePhase7();
+  void doExecutePhase8();
+  void doExecutePhase9();
+  void doFinish();
+
+  bool isPhase1Finished();
+  bool isPhase2Finished();
+  bool isPhase3Finished();
+  bool isPhase4Finished();
+  bool isPhase5Finished();
+  bool isPhase6Finished();
+  bool isPhase7Finished();
+  bool isPhase8Finished();
+  bool isPhase9Finished();
+};
