85a89aac45287a73d05b7b4ddfaf05546a94adce
Pragma <raph17.poth@gmail.com>
2024-01-19 18:20:58 -0500

    Insertion des objets mDriveTrain et mIMU

---
+++ Compets/2024/2024_StanRobotix_FRC/src/main/cpp/commands/TurnLeft.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/TurnLeft.h"
+
+TurnLeft::TurnLeft() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void TurnLeft::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void TurnLeft::Execute() {}
+
+// Called once the command ends or is interrupted.
+void TurnLeft::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool TurnLeft::IsFinished() {
+  return false;
+}
