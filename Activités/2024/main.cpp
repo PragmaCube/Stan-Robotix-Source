ef72fa2c1aa52fde0578774b3358d1413c49bbe9
Stan-Robotix <stanrobotix@stanislas.qc.ca>
2024-10-18 19:20:08 -0400

    Ajout d'un message

--- Activités/2024/main.cpp
+++ Activités/2024/main.cpp
@@ -5,6 +5,7 @@ int main()
     std::cout << "Allo du groupe 3 (Nous incluons tout le monde) ! ";
     std::cout << "Nayl est pas beau" ;
     std::cout << "TiMotHéE¨¨¨¨¨¨¨" ;
+    std::cout << "Allo du groupe 2!";
 
     
     return 0;
