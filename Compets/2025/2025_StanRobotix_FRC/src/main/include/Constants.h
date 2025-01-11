a6eeb14a11f84dacbadf41bebd4772f72e760243
StanRobotix <stan.robotix@stanislas.qc.ca>
2025-01-10 22:09:12 -0500

    Changed CAN ID (clockwise)

--- Compets/2025/2025_StanRobotix_FRC/src/main/include/Constants.h
+++ Compets/2025/2025_StanRobotix_FRC/src/main/include/Constants.h
@@ -22,17 +22,17 @@ inline constexpr int kDriverControllerPort = 0;
 
 
 namespace DriveTrainConstants {
-    constexpr int kBackRightMotorID = 8; // done
-    constexpr int kBackRightMotor550ID = 1; // done
-    constexpr int kBackLeftMotorID = 2; // done
-    constexpr int kBackLefttMotor550ID = 7; // done
-    constexpr int kFrontRighttMotorID = 4; // done
-    constexpr int kFrontRightMotor550ID = 3; // done
-    constexpr int kFrontLeftMotorID = 6; // done   
-    constexpr int kFrontLeftMotor550ID = 5; // done
+    constexpr int kBackRightMotorID = 8; 
+    constexpr int kBackRightMotor550ID = 7; 
+    constexpr int kFrontRighttMotorID = 6; 
+    constexpr int kFrontRightMotor550ID = 5; 
+    constexpr int kFrontLeftMotorID = 4;    
+    constexpr int kFrontLeftMotor550ID = 3;
+    constexpr int kBackLeftMotorID = 2; 
+    constexpr int kBackLefttMotor550ID = 1;  
 
     namespace PIDs{
-        constexpr double kP = 0.5;
+        constexpr double kP = 0.2;
         constexpr double kI = 0.0;
         constexpr double kD = 0.0;
     }
