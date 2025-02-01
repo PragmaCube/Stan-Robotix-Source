f6f0c4755bc6eb89930fc1a9c5123d68abaa9b47
Stan-Robotix <stanrobotix@stanislas.qc.ca>
2025-01-31 19:05:32 -0500

    Création du subsystem mecanumdrive pour la limelight

---
+++ Compets/2025/2025_StanRobotix_FRC/src/main/cpp/RobotContainer.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "RobotContainer.h"
+
+#include <frc2/command/button/Trigger.h>
+
+#include "commands/Autos.h"
+#include "commands/ExampleCommand.h"
+#include <frc2/command/RunCommand.h>
+
+RobotContainer::RobotContainer() {
+  // Initialize all of your commands and subsystems here
+
+  // Configure the button bindings
+  mIMU = new SubIMU;
+  mSub = new SubDriveTrain;
+  mIMU->ResetAngle();
+
+  mSub->SetDefaultCommand(frc2::RunCommand(
+        [this] {
+          mSub->drive(joystick.GetX(), joystick.GetY(), joystick.GetZ(), mIMU->getRotation2d());
+         },
+         {mSub}));
+
+
+  ConfigureBindings();
+}
+
+void RobotContainer::ConfigureBindings() {
+  // Configure your trigger bindings here
+
+  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+  frc2::Trigger([this] {
+    return m_subsystem.ExampleCondition();
+  }).OnTrue(ExampleCommand(&m_subsystem).ToPtr());
+
+ frc2::Trigger([this] {
+    return joystick.GetRawButtonPressed(3);
+  }).OnTrue(frc2::RunCommand([this] {mIMU->ResetAngle();},{mIMU}).ToPtr());
+
+  // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
+  // pressed, cancelling on release.
+  m_driverController.B().WhileTrue(m_subsystem.ExampleMethodCommand());
+}
+
+frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
+  // An example command will be run in autonomous
+  return autos::ExampleAuto(&m_subsystem);
+}
