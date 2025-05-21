9339fabbb63804b299578d6fa0517f08ddb66ee6
Stan-Robotix <stanrobotix@stanislas.qc.ca>
2025-05-21 18:06:45 -0400

    Ajout des LimitSwitch dans le dosier Activités

---
+++ Activités/2025/LimitSwitch_RnD/src/main/include/Constants.h
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <units/length.h>
+#include <units/angle.h>
+#include <units/velocity.h>
+#include <numbers>
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants.  This should not be used for any other
+ * purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+namespace OperatorConstants {
+
+inline constexpr int kDriverControllerPort = 0;
+
+}  // namespace OperatorConstants
+
+namespace LimitSwitchs {
+    constexpr int Port1 = 0;
+    constexpr int Port2 = 1;
+}
