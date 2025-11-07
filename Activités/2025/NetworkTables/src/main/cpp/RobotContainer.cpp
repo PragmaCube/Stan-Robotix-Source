001225a4d398e64ca57f87b0f7ca4e8d36b55aad
Stan Robotix <stanrobotix@stanislas.qc.ca>
2025-11-07 16:33:30 -0500

    Début du projet NetworkTables

---
+++ Activités/2025/NetworkTables/src/main/cpp/RobotContainer.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "RobotContainer.h"
+
+#include <frc2/command/button/Trigger.h>
+#include <frc2/command/RunCommand.h>
+
+RobotContainer::RobotContainer() {
+  // Initialize all of your commands and subsystems here
+
+  m_commandJoystick = new frc2::CommandJoystick{0};
+  m_commandXbox = new frc2::CommandXboxController{1};
+
+  mIMU = new SubIMU;
+  mDriveTrain = new SubDriveTrain{mIMU};
+
+  // mDriveTrain->SetDefaultCommand(frc2::RunCommand(
+  //     [this] {
+  //     mDriveTrain->driveFieldRelative(-m_commandJoystick->GetHID().GetX(),
+  //                                     -m_commandJoystick->GetHID().GetY(),
+  //                                     -m_commandJoystick->GetHID().GetZ(),
+  //                                     (-(m_commandJoystick->GetHID().GetThrottle()) / 2) + 0.5);
+  //     },
+  //     {mDriveTrain}));
+
+  mDriveTrain->SetDefaultCommand(frc2::RunCommand(
+      [this] {
+      mDriveTrain->driveFieldRelative(-m_commandXbox->GetLeftX (),
+                                      -m_commandXbox->GetLeftY (),
+                                      -m_commandXbox->GetRightY (),
+                                      (0.2));
+      },
+      {mDriveTrain}));
+
+  ConfigureBindings();
+
+  mIMU->resetAngle();
+
+  mTabGeneral->AddCamera("camera Tab","Limelight + usb",std::span<const std::string>({ "http://10.66.22.11:5800/" })).WithWidget(frc::BuiltInWidgets::kCameraStream);
+  mTabGeneral->Add("GoToTag",false);
+
+  // mRobotPose = new wpi::SendableBuilder{}
+
+  // mDriveTrain->InitSendable();
+}
+
+void RobotContainer::ConfigureBindings() {
+  m_commandJoystick->Button(JoystickBindingsConstants::kResetIMU).OnTrue(frc2::RunCommand([this] {mIMU->resetAngle();}, {mIMU}).ToPtr());
+}
+
+frc2::CommandPtr RobotContainer::GetAutonomousCommand(Auto iStartingPoint) {
+  // An example command will be run in autonomous
+
+  switch (iStartingPoint)
+  {
+  case BleuGauche:
+    return pathplanner::PathPlannerAuto("Bleu Gauche").ToPtr();
+    break;
+
+  case BleuCentre:
+    return pathplanner::PathPlannerAuto("Bleu Centre").ToPtr();
+    break;
+
+  case BleuDroite:
+    return pathplanner::PathPlannerAuto("Bleu Droite").ToPtr();
+    break;
+  
+  case RougeGauche:
+    return pathplanner::PathPlannerAuto("Rouge Gauche").ToPtr();
+    break;
+
+  case RougeCentre:
+    return pathplanner::PathPlannerAuto("Rouge Centre").ToPtr();
+    break;
+
+  case RougeDroite:
+    return pathplanner::PathPlannerAuto("Rouge Droite").ToPtr();
+    break;
+
+  case Test:
+    return pathplanner::PathPlannerAuto("Test").ToPtr();
+    break;
+
+  default:
+    return pathplanner::PathPlannerAuto("Rouge Gauche").ToPtr();
+    break;
+  }
+}
---
+++ Activités/2025/NetworkTables/src/main/include/subsystems/SubDriveTrain.h
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <pathplanner/lib/auto/AutoBuilder.h>
+#include <pathplanner/lib/config/RobotConfig.h>
+#include <pathplanner/lib/controllers/PPHolonomicDriveController.h>
+#include <frc2/command/SubsystemBase.h>
+#include <frc/geometry/Translation2d.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <frc/kinematics/SwerveModulePosition.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/DriverStation.h>
+#include <frc/Joystick.h>
+#include <tuple>
+#include <array>
+#include <vector>
+#include <units/velocity.h>
+#include <units/angle.h>
+#include <wpi/array.h>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/DoubleTopic.h>
+#include <networktables/StringArrayTopic.h>
+#include <networktables/StructArrayTopic.h>
+
+#include "Constants.h"
+#include "subsystems/SubIMU.h"
+#include "subsystems/SwerveModule.h"
+
+class SubDriveTrain : public frc2::SubsystemBase {
+ public:
+  SubDriveTrain(SubIMU*);
+
+  enum StartPoses{
+    RougeGauche,
+    RougeCentre,
+    RougeDroite,
+    BleuGauche,
+    BleuCentre,
+    BleuDroite,
+    Test
+  };
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+// Method that drives the robot in field relative drive
+  void driveFieldRelative(float iX, float iY, float i0, double SpeedModulation);
+
+// Method that returns a ChassisSpeeds from the robot relative speeds
+  frc::ChassisSpeeds getRobotRelativeSpeeds();
+// Method that drives the robot in robot relative drive
+  void driveRobotRelative(frc::ChassisSpeeds speeds, double SpeedModulation);
+
+// Method that returns the robot's pose
+  frc::Pose2d getPose();
+// Method that redefines the robot's pose with its input
+  void resetPose(frc::Pose2d iRobotPose);
+
+  void Init();
+
+  // Load the RobotConfig from the GUI settings. You should probably
+  // store this in your Constants file
+  pathplanner::RobotConfig config = pathplanner::RobotConfig::fromGUISettings();
+
+ private:
+
+  // Declaring the locations of the SwerveModules
+  frc::Translation2d * m_frontLeftLocation;
+  frc::Translation2d * m_frontRightLocation;
+  frc::Translation2d * m_backLeftLocation;
+  frc::Translation2d * m_backRightLocation;
+
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  // nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
+  // std::shared_ptr<nt::NetworkTable> table = inst.GetTable("Swerve");
+  // // std::array<nt::StructArrayTopic(table), 4> stuctArrayTopic;
+  // nt::StructArrayTopic<frc::SwerveModuleState> m_moduleStatesTopic;
+  // // std::array<nt::StructArrayPublisher, 4> m_moduleStatePublisher;
+  // nt::StructArrayPublisher<frc::SwerveModuleState> m_moduleStatesPublisher;
+
+  // Declaring the four SwerveModule objects
+  SwerveModule * m_frontLeftModule;
+  SwerveModule * m_frontRightModule;
+  SwerveModule * m_backLeftModule;
+  SwerveModule * m_backRightModule;
+
+  // wpi::array<frc::SwerveModulePosition, 4> * m_swerveModulePositions;
+
+  // Declaring my swerve kinematics object
+  frc::SwerveDriveKinematics<4> * m_kinematics;
+  // Declaring the robot pose object
+  frc::Pose2d * m_robotPose;
+  // Declaring the swerve odometry object
+  frc::SwerveDriveOdometry<4> * m_odometry;
+
+  // Declaring the IMU object
+  SubIMU * mIMU = nullptr;
+
+  frc::Pose2d CoordonneesInit;
+  StartPoses StartPose = RougeCentre;
+};
