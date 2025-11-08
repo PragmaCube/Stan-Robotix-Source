4b2dde93bb7c26e6bd082d87d958862720e0f5d9
StanRobotix <stan.robotix@stanislas.qc.ca>
2025-11-07 19:42:07 -0500

    Adding Allo class to Groupe 2-2

---
+++ Activités/2025/2025_StanRobotix_Github/Class_Allo.cpp
@@ -0,0 +1,17 @@
+class Allo
+{
+    int mAllo = 3232;
+public:
+    Allo();
+    ~Allo();
+    int getAllo();
+};
+
+Allo::Allo() { }
+
+Allo::~Allo() { }
+
+int Allo::getAllo()
+{
+    return mAllo;
+}
