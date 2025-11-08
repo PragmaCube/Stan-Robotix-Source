fbd8bf7506d1a4711cdcef87591123759dd0acf6
Alekz <alexanderalfaleh@gmail.com>
2025-11-07 19:00:18 -0500

    changement de main.cpp
    je n'ai pas reussi de commit plusieurs fichiers

--- Activités/2025/2025_StanRobotix_Github/main.cpp
+++ Activités/2025/2025_StanRobotix_Github/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cmath>
 
+#include "constants.h"
+
 int main()
 {
-    std::cout << "La racine carrée de 2 vaut environ : " << std::sqrt(2.0);
-
+    std::cout << pi::pi << std::endl;
     return 0;
 }
