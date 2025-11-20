786f4dba5c3330e851994c94e4e1ba861d847477
Stan Robotix <stanrobotix@stanislas.qc.ca>
2025-11-19 19:42:29 -0500

    Creation des controlleurs de moteurs et creation de MotorControllerGroup.

--- Activités/2025/Groupe 2 Robot/src/main/include/subsystems/Drivetrain.h
+++ Activités/2025/Groupe 2 Robot/src/main/include/subsystems/Drivetrain.h
@@ -5,7 +5,10 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
-#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/drive/DifferentialDrive.h>
+#include "Constants.h"
+#include <frc/motorcontrol/MotorControllerGroup.h>
 
 class Drivetrain : public frc2::SubsystemBase {
  public:
@@ -20,8 +23,14 @@ class Drivetrain : public frc2::SubsystemBase {
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 
-  ctre::phoenix::motorcontrol::can::TalonSRX moteur1{1};
-  ctre::phoenix::motorcontrol::can::TalonSRX moteur2{2};
-  ctre::phoenix::motorcontrol::can::TalonSRX moteur3{3};
-  ctre::phoenix::motorcontrol::can::TalonSRX moteur4{4};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* moteur1{DriveTrainConstants::moteur1ID};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* moteur2{DriveTrainConstants::moteur2ID};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* moteur3{DriveTrainConstants::moteur3ID};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* moteur4{DriveTrainConstants::moteur4ID};
+frc::DifferentialDrive* Drive;
+	MotorControllerGroup
+  (ctre::phoenix::motorcontrol::can::WPI_TalonSRX &moteur1, 
+   ctre::phoenix::motorcontrol::can::WPI_TalonSRX &moteur2, 
+  );
+
 };
