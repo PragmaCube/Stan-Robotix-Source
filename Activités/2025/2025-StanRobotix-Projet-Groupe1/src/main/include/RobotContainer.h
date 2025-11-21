99853e9b840b8dd336c52b90b12d81b2907a5f96
Paul-Hugo Fortune <you@example.copolux988@gmail.com>
2025-11-21 18:49:15 -0500

    Implementation of XboxController method to test

--- Activités/2025/2025-StanRobotix-Projet-Groupe1/src/main/include/RobotContainer.h
+++ Activités/2025/2025-StanRobotix-Projet-Groupe1/src/main/include/RobotContainer.h
@@ -29,14 +29,13 @@ class RobotContainer {
 
  private:
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  frc2::CommandXboxController m_driverController{
-      OperatorConstants::kDriverControllerPort};
+  frc2::CommandXboxController m_driverController{OperatorConstants::kDriverControllerPort};
 
   // The robot's subsystems are defined here...
   ExampleSubsystem m_subsystem;
   Drivetrain * m_drivetrain;
 
-  frc::XboxController(0);
+  frc::XboxController m_driveXboxController{0};
   void ConfigureBindings();
 
 };
