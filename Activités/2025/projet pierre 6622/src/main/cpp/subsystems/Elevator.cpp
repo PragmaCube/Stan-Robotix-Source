31258ba1ecc198de4444f3aee8066037eb1f0162
Stan Robotix <stanrobotix@stanislas.qc.ca>
2026-01-07 19:34:27 -0500

    commencé Elevator.cpp et Elevator.h

---
+++ Activités/2025/projet pierre 6622/src/main/cpp/subsystems/Elevator.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Elevator.h"
+
+Elevator::Elevator() = default;
+
+void Elevator::Up()
+{
+
+}
+
+void Elevator::Down()
+{
+
+}
+
+void Elevator::Stop()
+{
+    
+}
+
+// This method will be called once per scheduler run
+void Elevator::Periodic() {}
