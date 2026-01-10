68cf7c161ac21126536173b1a0245c27eef73ccb
Starelie <timotheelaberge@gmail.com>
2026-01-10 11:56:25 -0500

    Changed some of the folder path

---
+++ Activites/2025/projet pierre 6622/src/main/cpp/RobotContainer.cpp
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "RobotContainer.h"
+
+#include <frc2/command/button/Trigger.h>
+
+#include "commands/Autos.h"
+#include "commands/ExampleCommand.h"
+
+#include <frc2/command/RunCommand.h>
+
+#include "commands/TurnRight.h"
+
+RobotContainer::RobotContainer() {
+  // Initialize all of your commands and subsystems here
+
+  m_Imu = new IMU;
+  m_Drivetrain = new Drivetrain;
+
+  // Configure the button bindings
+  ConfigureBindings();
+}
+
+void RobotContainer::ConfigureBindings() {
+  // Configure your trigger bindings here
+
+  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+  frc2::Trigger([this] {
+    return m_subsystem.ExampleCondition();
+  }).OnTrue(ExampleCommand(&m_subsystem).ToPtr());
+
+  frc2::Trigger([this] {
+  return m_XboxController.GetAButtonPressed();
+  }).OnTrue(TurnRight(m_Imu, m_Drivetrain).ToPtr());
+
+
+  // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
+  // pressed, cancelling on release.
+  m_driverController.B().WhileTrue(m_subsystem.ExampleMethodCommand());
+
+    m_Drivetrain->SetDefaultCommand(frc2::RunCommand(
+    [this]
+    {
+      m_Drivetrain->Drive(m_XboxController.GetLeftY(),m_XboxController.GetRightY());
+
+    },
+    {m_Drivetrain}));
+
+    //m_Imu.SetDefaultCommand(frc2::RunCommand(
+    //[this]
+    //{
+    //  m_Drivetrain.(m_XboxController.GetLeftY(),m_XboxController.GetRightY());
+
+    //},
+    //{&m_Drivetrain}));
+}
+
+frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
+  // An example command will be run in autonomous
+  return autos::ExampleAuto(&m_subsystem);
+}
